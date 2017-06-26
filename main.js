c9aa0cb937b7acba0cd2673dfbade37fdd300e2e
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-26 10:50:40 +0530

    adding js

---
+++ main.js
@@ -0,0 +1,8 @@
+//envoked when the user clicks the add button
+document.getElementById('add').addEventListener('click',function(){
+var value= document.getElementById('item') .value;
+console.log(value);
+if(value){
+    
+}
+});
